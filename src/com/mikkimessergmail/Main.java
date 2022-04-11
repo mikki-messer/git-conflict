af8d564266adbc2b5c5ce91272002c7b27d70265
Mike Bulanov <mikki.messer@gmail.com>
2022-04-11 22:09:18 +0300

    Exclamation mark

--- src/com/mikkimessergmail/Main.java
+++ src/com/mikkimessergmail/Main.java
@@ -3,6 +3,6 @@ package com.mikkimessergmail;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("This is the first line");
+	    System.out.println("This is the first line!");
     }
 }
