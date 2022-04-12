6a6ed81022d3cd47719778a123dbf6226f4c1058
Mike Bulanov <mikki.messer@gmail.com>
2022-04-12 11:12:37 +0300

    Second line added

--- src/com/mikkimessergmail/Main.java
+++ src/com/mikkimessergmail/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("This is the first line!");
+        System.out.println("This is the 2econd line!");
     }
 }
